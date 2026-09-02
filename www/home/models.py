017e2186da460dffb755c8c15218a2f3afa30054
Chris Mann <chris@lesgtrandsvoisins.com>
2026-09-02 15:36:38 +0200

    Une première page de contenu

--- www/home/models.py
+++ www/home/models.py
@@ -2,8 +2,22 @@ from django.db import models
 
 from wagtail.models import Page
 
+from wagtail.fields import RichTextField
+from wagtail.search import index
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
+
 class HomePage(Page):
     pass
 
+# Ajouté par Chris
 class ContentPage(Page):
-    pass
+    
+    body = RichTextField(null=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField("body"),
+    ]
+    
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
