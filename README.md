a3be3daba0a61c300fa44464ef0934113d9e5fa2
Chris Mann <chris@lesgtrandsvoisins.com>
2026-09-02 15:11:04 +0200

    Site vierge initial

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # gvcooporg
+
 Site Web www.gvcoop.org
+
+Nouveau site [wagtail.org](https://wagtail.org) destiné à [www.gvcoop.org](https://www.gvcoop.org).
+
+
+
+```
+python -m venv .venv
+source .venv/bin/activate
+pip install wagtail
+cd www
+pip install -r requirements.txt
+pip install --upgrade pip
+python manage.py migrate
+python manage.py createsuperuser
+
+```
---
+++ www/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-v-xc^xgt-ad4y_r9$dcj#az3=wbt(nca6-gxp_uijki-+50!=g"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
